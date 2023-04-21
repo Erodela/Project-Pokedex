b2cd20aa955a41b08b5850842be1fe35fab714e5
Edwin <edwinrodela@gmail.com>
2023-04-21 05:11:28 -0500

    Day 5 Commit 3

--- src/pages/Type.js
+++ src/pages/Type.js
@@ -1,4 +1,5 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
+// import { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
 
